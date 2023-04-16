856e9b91bc1afb679e734328bb5bca177408c2f4
Nguyen The Man <hoangtupro.a4@gmail.com>
2023-04-16 17:50:56 +0700

    add entrypoint.sh

---
+++ entrypoint.sh
@@ -0,0 +1,6 @@
+php -r "copy('https://getcomposer.org/installer', 'composer-setup.php');"
+php composer-setup.php
+php -r "unlink('composer-setup.php');"
+php composer.phar require  --dev phpunit/phpunit
+php composer.phar install
+./vendor/bin/phpunit --coverage-text
