053485b3da3d463e6823c85701bf046bb2c82d05
Nguyen The Man <hoangtupro.a4@gmail.com>
2023-04-21 22:44:51 +0700

    fix sh file

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,7 +1,7 @@
-php -v
-php -r "copy('https://getcomposer.org/installer', 'composer-setup.php');"
-php composer-setup.php
-php -r "unlink('composer-setup.php');"
-php composer.phar require  --dev phpunit/phpunit
-php composer.phar install
-./vendor/bin/phpunit --coverage-text
+docker exec -it --tty=false my-docker-image php -v
+docker exec -it --tty=false my-docker-image php -r "copy('https://getcomposer.org/installer', 'composer-setup.php');"
+docker exec -it --tty=false my-docker-image php composer-setup.php
+docker exec -it --tty=false my-docker-image php -r "unlink('composer-setup.php');"
+docker exec -it --tty=false my-docker-image php composer.phar require  --dev phpunit/phpunit
+docker exec -it --tty=false my-docker-image php composer.phar install
+docker exec -it --tty=false my-docker-image ./vendor/bin/phpunit --coverage-text
